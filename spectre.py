3921581d60db57ad7c5eca0b41464a376e062476
George Budd <gbudd@ironkeyboard.com>
2018-04-22 21:10:22 -0400

    autopep8

--- spectre.py
+++ spectre.py
@@ -3,24 +3,22 @@
 import requests
 import json
 
+
 class Server():
     session = requests.Session()
-    server = "";
-    url = "";
-    cookies = {};
+    server = ""
+    url = ""
+    cookies = {}
 
     def __init__(self, server, api_key):
-        self.session.headers = {'Authorization':"Bearer " + api_key,
-                'Accept':'json;pretty'}
+        self.session.headers = {'Authorization': "Bearer " + api_key,
+                                'Accept': 'json;pretty'}
         self.session.verify = False
         self.url = "https://" + server + "/api/rest/"
-        self.session.timeout = 1;
+        self.session.timeout = 1
 
-    def get(self,suffix,inparams):
-        return self.session.get(self.url+suffix,params=inparams)
-                
-                
-                
-                
-s=Server("i3","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRlIjoxNTI0NDI5ODU2NTA2LCJ1c2VyIjoiYWRtaW4ifQ.KEaRBjPVMnsdPAG6l3oinHOjPFAfsfUkgOs0YKyhwds")
+    def get(self, suffix, inparams):
+        return self.session.get(self.url+suffix, params=inparams)
 
+
+s = Server("i3", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRlIjoxNTI0NDI5ODU2NTA2LCJ1c2VyIjoiYWRtaW4ifQ.KEaRBjPVMnsdPAG6l3oinHOjPFAfsfUkgOs0YKyhwds")
