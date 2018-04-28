90f8a8418dc2adc6bd030c0645c59707482bd776
George Budd <gbudd@ironkeyboard.com>
2018-04-28 15:08:59 -0400

    Start with an introductory doctest

--- spectre.py
+++ spectre.py
@@ -6,7 +6,7 @@ import json
 
 class Server():
     """
-    A Server is used to make API Calls to a Lumeta Spectre server 
+    A Server is used to make API Calls to a Lumeta Spectre(r) server 
     """
 
     def __init__(self, server):
@@ -15,11 +15,12 @@ class Server():
         self.url = "https://" + server + "/api/rest/"
         self.session.timeout = 1
 
-    def get(self, api, params=None):
+    def _get(self, api, params=None):
         """
         """
-        r = self.session.get(self.url+api, params=params,timeout=5)
+        r = self.session.get(self.url+api, params=params, timeout=5)
         return r
+
 
 class APIKeyServer(Server):
     """
@@ -27,26 +28,33 @@ class APIKeyServer(Server):
     You get an API key from the CLI via the "user key new <username>" command
     """
 
-    def __init__(self,server,api_key):
-            """
-            APIKeyServer(server,api_key) where
-            server is the Spectre server you're connecting to and
-            api_key is the API key you've generated
-            """
-            super().__init__(server)
-            self.session.headers = {'Authorization': "Bearer " + api_key,
-                                    'Accept': 'json;pretty'}
+    def __init__(self, server, api_key):
+        """
+        APIKeyServer(server,api_key) where
+        server is the Spectre server you're connecting to and
+        api_key is the API key you've generated
+
+        >>> import spectre
+        >>> s = APIKeyServer("i3", api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRlIjoxNTI0NDI5ODU2NTA2LCJ1c2VyIjoiYWRtaW4ifQ.KEaRBjPVMnsdPAG6l3oinHOjPFAfsfUkgOs0YKyhwds")
+        >>> r = s._get("system/information")
+        >>> r.json()['status']
+        'SUCCESS'
+        >>> r.json()['results'][0]['name']
+        'i3'
+        """
+        super().__init__(server)
+        self.session.headers = {'Authorization': "Bearer " + api_key,
+                                'Accept': 'json;pretty'}
 
 
+if __name__ == '__main__':
 
-if __name__ == '__main__': 
-
-    s = APIKeyServer("i3", api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRlIjoxNTI0NDI5ODU2NTA2LCJ1c2VyIjoiYWRtaW4ifQ.KEaRBjPVMnsdPAG6l3oinHOjPFAfsfUkgOs0YKyhwds")
-    r=s.get("system/information")
+    s = APIKeyServer(
+        "i3", api_key="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRlIjoxNTI0NDI5ODU2NTA2LCJ1c2VyIjoiYWRtaW4ifQ.KEaRBjPVMnsdPAG6l3oinHOjPFAfsfUkgOs0YKyhwds")
+    r = s.get("system/information")
     print(r.headers)
     print(r.text)
 
-    r=s.get("zonedata/devices",params={"filter.zone.id":"4"})
+    r = s.get("zonedata/devices", params={"filter.zone.id": "4"})
     print(r.headers)
     print(r.text)
-
