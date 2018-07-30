230c71a2df0a93cb56563c717331bdcb9c3ac8bb
George Budd <gbudd@ironkeyboard.com>
2018-07-29 22:33:26 -0400

    Update cloud test

--- tests/test_cloud.py
+++ tests/test_cloud.py
@@ -3,6 +3,6 @@
 def test_awsdevice(server):
     '''Test what an AWS device looks like'''
     zone = server.get_zone_by_name('Twilight')
-    results = zone.get_device_details_by_ip('1.1.1.1')
+    results = zone.query().filter('address.ip','1.1.1.1')
     assert not results.values(), "There shouldn't be any results if we don't find the IP"
 
